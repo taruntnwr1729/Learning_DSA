c6ff6c773d1d4124b9ebcb7dec27eea0b5954c12
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-06-11 00:43:20 +0530

    Add LinkedList.cpp with array-to-list conversion
    Create DataStructures/LinkedList.cpp: defines a simple Node struct and ConvertArr2LL(vector<int>&) to build a singly linked list from a vector. Includes a main that constructs a sample list ({2,3,4,5,1,6,7}) and prints the third node's value. Note: no empty-vector checks or memory cleanup/destructor are implemented in this demo.

---
+++ DataStructures/LinkedList.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* ConvertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i = 1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+
+
+int main(){
+
+    vector <int> arr = {2,3,4,5,1,6,7};
+    Node* head = ConvertArr2LL(arr);
+    cout<<head->next->next->data;
+    return 0;
+}
