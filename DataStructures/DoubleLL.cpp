e16fa5c8aeec5d5d5173fc4c5285f36e5826f62c
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-06-13 16:04:42 +0530

    Add array->DLL conversion and rename prev to back
    Include <vector>, rename Node::prev to Node::back (and update usages), and add a three-argument Node constructor. Implement convertArr2DLL(vector<int>&) to build a doubly-linked list from a vector and update printBackword to traverse via back. Update main to create a sample vector, convert it to a DLL, and print the list.

--- DataStructures/DoubleLL.cpp
+++ DataStructures/DoubleLL.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 
 struct Node {
     public:
     int data;
     Node* next;
-    Node* prev;
+    Node* back;
+
+    public:
+    Node(int data1, Node* next1, Node* prev1){
+        data= data1;
+        next = next1;
+        back = prev1;
+    }
 
     public:
     Node(int data1){
         data = data1;
         Node* next = NULL;
-        Node* prev = NULL;
+        Node* back = NULL;
 
     }
+
 };
 
 void printForward(Node* head){
@@ -27,15 +37,31 @@ void printForward(Node* head){
 void printBackword(Node* head){
     while(head!=NULL){
         cout<<head->data<<" ";
-        head= head->prev;
+        head= head->back;
     }
     cout<<endl;
 }
 
+Node* convertArr2DLL(vector <int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+
+    for(int i = 1; i<arr.size();i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+
+
+    return head;
+
+}
 
 
 int main(){
-
-
+    vector <int> arr = {2,3,4,5,1,6,7};
+    Node* head = convertArr2DLL(arr);
+    printForward(head);
+    
     return 0;
 }
