dbded72a128978ee29bc0bcd0f987775c185bcdf
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-06-15 03:10:04 +0530

    Fix insertion sort loop start and formatting
    Start outer loop from i = 1 (was i = 0) to correctly perform insertion sort. Clean up whitespace/indentation and standardize spacing in comparisons and assignments. Add trailing newline after printed output for cleaner console output. No algorithmic logic changed aside from the corrected loop start.

--- Basic Sorting/InsertionSort.cpp
+++ Basic Sorting/InsertionSort.cpp
@@ -2,30 +2,33 @@
 using namespace std;
 
 void InsertionSort(int arr[], int n){
-
-    for(int i = 0; i<n-1; i++){
-        int j=i;
-        while(j>0 && arr[j-1]>arr[j]){
+    
+    for(int i = 1; i < n; i++){
+        int j = i;
+        
+        
+        while(j > 0 && arr[j-1] > arr[j]){
+            
             int temp = arr[j];
-            arr[j]=arr[j-1];
-            arr[j-1]=temp;
-            j--;
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            
+            j--; 
         }
     }
-
+    
 }
 
-
 int main(){
-
-    int arr[] = {5,4,3,2,1};
+    int arr[] = {5, 4, 3, 2, 1};
     int n = sizeof(arr) / sizeof(arr[0]); 
 
     InsertionSort(arr, n);
 
-    for(int j=0; j<n; j++){
-        cout<<arr[j]<<" ";
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << " ";
     }
+    cout << endl;
 
     return 0;
 }
