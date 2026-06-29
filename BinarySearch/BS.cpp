403c3d3cd674bbc12fc77b3938c0218af1563964
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-06-29 23:25:44 +0530

    Add upper_bound example and update VSCode settings
    Add BinarySearch/BS.cpp implementing an upperbound binary-search example that prints the resulting index and value. Update .vscode/settings.json: change editor.hover.enabled to "off" and enable C_Cpp.errorSquiggles to surface C/C++ diagnostics in the editor.

---
+++ BinarySearch/BS.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int upperbound(vector<int> &arr, int x, int n ){
+    int low = 0, high = n - 1;
+    int ans = n;
+    while(low<=high){
+        int mid = low + (high - low)/2;
+
+        if(arr[mid]>x){
+            ans = mid;
+            high = mid - 1;
+        }else{
+            low = mid+1;
+        }
+
+    }
+    
+    return ans;
+
+}
+
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7,7,8,9,10,11,11,12,13};
+    int n = arr.size();
+    int x = 9;
+    int index = upperbound(arr, x, n);
+
+    cout<<"The upperbound of "<< x <<" is at index: "<<index<<endl;
+    cout<<"The value at that index is:" << arr[index]<<endl;
+
+    return 0;
+}
