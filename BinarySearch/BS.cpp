1e57980b2a235974f2574e9d2d3e9a38f85c7afe
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-06-30 19:05:11 +0530

    Add RangeofEl function and fix variable names
    Added RangeofEl() function that returns the range [lower, upper] of consecutive occurrences of an element using lowerbound and upperbound. Fixed incorrect variable names in cout statements (changed undefined 'index' to 'indexUP' and 'indexLOW'). Included test case in main().

--- BinarySearch/BS.cpp
+++ BinarySearch/BS.cpp
@@ -40,18 +40,27 @@ int lowerbound(vector<int> &arr, int x, int n ){
 
 }
 
+//IF SOMEONE ASKS ME TO TELL THE RANGE OF THE SAME ELEMENT OCCURING CONSECUTEVILY WE CAN TELL IT BY USING LOWER BOUND AND UPPERBOUND
+pair<int,int>RangeofEl(vector<int> &arr, int x, int n){
+    int lb = lowerbound(arr, x, n);
+    if(lb==n || arr[lb] != x) return {-1, -1};
+    return {lb, (upperbound(arr,x,n)-1)};
 
+}
 int main(){
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7,7,8,9,10,11,11,12,13};
     int n = arr.size();
     int x = 9;
     int indexUP = upperbound(arr, x, n);
     int indexLOW = lowerbound(arr, x, n);
-    cout<<"The upperbound of "<< x <<" is at index: "<<index<<endl;
+    cout<<"The upperbound of "<< x <<" is at index: "<<indexUP<<endl;
     cout<<"The value at that index is:" << arr[indexUP]<<endl;
 
-    cout<<"The lowerbound of "<< x <<" is at index: "<<index<<endl;
+    cout<<"The lowerbound of "<< x <<" is at index: "<<indexLOW<<endl;
     cout<<"The value at that index is:" << arr[indexLOW]<<endl;
+
+    pair<int, int> range = RangeofEl(arr, x, n);
+    cout << "Range is: [" << range.first << ", " << range.second << "]" << endl;
 
     return 0;
 }
