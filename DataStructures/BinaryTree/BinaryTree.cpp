10804eb75f7f8cbd2f2e125aed3a03695476dce1
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-07-02 00:16:56 +0530

    Add binary tree traversal functions
    Implement Preorder, Inorder, and Postorder traversal functions for binary trees. Added test calls in main() to demonstrate all three traversal methods.

--- DataStructures/BinaryTree/BinaryTree.cpp
+++ DataStructures/BinaryTree/BinaryTree.cpp
@@ -30,12 +30,49 @@ int maxDepth(Node* root){
         return 1 + max(lh,rh);
 }
 
+void Preorder(Node* root){
+    if(root==NULL){
+        return ;
+
+    }
+    cout<<root->data<<" ";
+    Preorder(root->left);
+    Preorder(root->right);
+}
+
+void Inorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    Inorder(root->left);
+    cout<<root->data<<" ";
+    Inorder(root->right);
+}
+
+void Postorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    Postorder(root->left);
+    Postorder(root->right);
+    cout<<root->data<<" ";
+}
 int main(){
     Node* root = new Node(10);
     root->left = new Node(20);
     root->right = new Node(30);
     root->right->left = new Node(50);
     root->left->right = new Node(70);
+    cout<<endl;
+    cout<<"Preorder: ";
+    Preorder(root);
+    cout<<endl;
+    cout<<"Inorder: ";
+    Inorder(root);
+    cout<<endl;
+    cout<<"Postorder: ";
+    Postorder(root);
+    cout<<endl;
     cout << "Maximum depth of the tree is: " << maxDepth(root) << endl;
 
     return 0;
