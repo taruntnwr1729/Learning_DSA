af5642505bd1884b36509d1ab67dc29477e294a0
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-07-18 20:28:32 +0530

    Add smallest divisor binary search solution
    Implement algorithm to find the smallest divisor such that the sum of ceiling divisions of array elements does not exceed a given limit. Uses binary search to efficiently find the answer.

---
+++ BinarySearch/BS_MediumProblems/SmallestDivisor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int SumByD(vector <int> arr, int div){
+    //THIS FUNCTION EXISTS SO THAT WE CAN
+    //CALCULATE IF DIVIDING THE ELEMENTS OF ARRAY BY A DIVISIOR
+    //AND ADDING THEM IS GIVING MINIMUM RESULT OR NOT
+    int sum = 0;
+    int n = arr.size();
+    for(int i = 0; i<n; i++){
+        sum = sum + ceil(double(arr[i])/double(div));
+    }
+
+    return sum;
+}
+
+int smallDivisor(vector <int> arr, int limit){
+
+    int low = 1; int high = *max_element(arr.begin(), arr.end());
+    while(low<=high){
+        int mid = low + (high - low)/2;
+        if(SumByD(arr,mid)<= limit){
+            high = mid - 1;
+
+        }else{
+            low = mid+1;
+        }
+    }
+
+    return low;
+}
