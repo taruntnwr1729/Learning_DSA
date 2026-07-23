0f8ed121174f6bf08fa570088bf3583ac0795cf6
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-07-23 22:12:18 +0530

    Add robust array rearrangement function
    Add a new Rearrange() function that handles rearranging arrays with positive and negative numbers even when their counts are unequal. The function separates elements into positive and negative vectors, then alternates them in the result array, appending any remaining elements. Also add a clarifying comment to the existing RearrangeElements() function noting its limitation.

--- DataStructures/Arrays/Rearrange.cpp
+++ DataStructures/Arrays/Rearrange.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+//this works only when no. of negatives and positives are same
 vector <int> RearrangeElements(const vector <int> arr){
     int n = arr.size();
     vector <int> ans(n,0);
@@ -20,4 +21,47 @@ vector <int> RearrangeElements(const vector <int> arr){
     }
     return ans;
     
+}
+
+vector <int> Rearrange( vector <int> arr){
+    int n = arr.size();
+    vector <int> pos;
+    vector <int> neg;
+
+    //with this loop we made positive and negative arrays
+    for(int i =0; i<n ; i++){
+        if(arr[i]>0){
+            pos.push_back(arr[i]);
+        }else{
+            neg.push_back(arr[i]);
+
+        }
+    }
+    
+    //then we checked which array's size is more
+    if(pos.size() > neg.size()){ //if positive is greater
+        for(int i = 0; i< neg.size(); i++){ //then we run the loop till negative array doesnt end
+            arr[2*i] = pos[i];  //we assign even position to positives
+            arr[2*i + 1] = neg[i];//and odd position to negatives
+        }
+        int index = neg.size()*2; //then we do this so that we get the position till the array has been filled
+        for(int i = neg.size(); i<pos.size(); i++){ //we start from negative size till positive arrays size
+            arr[index] = pos[i]; //assign elements in array;
+            index++;
+        }
+    }else{
+        for(int i = 0; i< pos.size(); i++){ //we do this if negative is greater
+            arr[2*i] = pos[i];
+            arr[2*i + 1] = neg[i];
+        }
+        int index = pos.size()*2;
+        for(int i = pos.size(); i<neg.size(); i++){
+            arr[index] = neg[i];
+            index++;
+        }
+
+    }
+
+    return arr;
+
 }
